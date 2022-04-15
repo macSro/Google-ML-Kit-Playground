4aa3618be0ec93bed0c9b90edaa41e5c16f40a0f
Maciej Sroczek <maciej.sroczek@gmail.com>
2022-04-15 20:26:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lab 3
+
+A Flutter App for Mobile And Multimedia Systems (PWr)
+
+- To run the app, execute: `flutter run`
---
+++ android/app/src/main/kotlin/pl/edu/pwr/lab3/i242450/lab3/MainActivity.kt
@@ -0,0 +1,6 @@
+package pl.edu.pwr.lab3.i242450.lab3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
